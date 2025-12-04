b891af44c9f4818c4eecc2c2ecebf45f4fdcfa5d
axelleB106 <axelleperg@gmail.com>
2025-12-04 18:31:30 +0900

    BASE_APP_Display

---
+++ FactoryInventoryToolManagement.java
@@ -0,0 +1,338 @@
+/*
+ * BOULANGER AXELLE 
+ * 25040201
+ * PROJECT JAVA 
+ * Factory Inventory & Tool Management System
+ */
+
+
+/*TO DO 
+- Install MySQL and connect 
+USE SWING 
+
+//Workshop -- 
+=================================================================
+ITEMs 
+Example items:
+Servo motors
+Microcontrollers (Arduino, ESP32…)
+Sensors (temperature, ultrasonic…)
+Robot arms & end-effectors
+Tools (screwdrivers, soldering iron…)
+- Item screnn - roll bar - choose item 
+insert new item type -- Quantity + Category
+
+
+USER - basic screen 
+-- User - start app version - either - test version with random number of items already in stock 
+-- or admin version where we choose number of stock 
+- add items 
+- edit items 
+- delete items 
+- search items 
+
+VISUALS 
+Highlight - low-stock items 
+- display inventory 
+sorting button 
+
+- alert of low stock 
+=================================================================
+	BASE Appearance done 
+CURRENT 
+-- ADD Btn to left side + text display just under 
+
+Item: 
+	- cdt for name and Qtt
+	- Base display 
+	- Save - close window 
+	- Connect with ADD Button
+	To do : 
+		- Send Info to SQL Data Base when Save clicked 
+		
+ADD Button 
+	-click - open calls Item class 
+	-  when item class closes == save btn 
+	- read from database and add - Item to display - 
+		- fct to only display the 6 Item wih the lowest qtt 
+	
+	
+*/
+
+package Assignments; 
+
+//My SQL imports 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+
+
+//region ITEM =================================================================================================
+class Item {
+	String name;
+	int id; //set-up later with auto_increment
+	int quantity; 
+	Item(){ //CONSTRUCTOR
+		//Open a page and ask to fill name etc..
+		JFrame ItemSetUp = new JFrame ("NEW ITEM");
+		ItemSetUp.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Dispose - else everything closes 
+		ItemSetUp.setLocation(1020, 300); 
+		
+		JLabel Name_Label = new JLabel("Enter Item Name");
+		JLabel Qtt_Label = new JLabel("Enter Item Quantity");
+		JTextField Name_textfield = new JTextField();
+		JTextField Qtt_textfield = new JTextField();
+		
+		Name_textfield.setColumns(10);
+		Qtt_textfield.setColumns(5);	
+		Name_textfield.setBorder(BorderFactory.createLineBorder(new Color(0xE2E6F5),2));
+		Qtt_textfield.setBorder(BorderFactory.createLineBorder(new Color(0xE2E6F5),2));
+		
+		//SAVE Button that save into the data BASE 
+		
+		JButton SAVE_btn = new JButton("SAVE");
+		SAVE_btn.setHorizontalTextPosition(SwingConstants.CENTER);
+		SAVE_btn.setBackground(new Color(0xE2E6F5));
+		SAVE_btn.setFocusPainted(false);
+		SAVE_btn.setBorderPainted(false);
+		
+		//--------------
+		SAVE_btn.addActionListener(e -> btnClick(Name_textfield, Qtt_textfield,ItemSetUp));
+		
+		//PRIMARY-----------------------------------
+		JPanel primary = new JPanel();
+		primary.setLayout(new GridLayout(3, 1, 5,5));
+		primary.setPreferredSize(new Dimension (250, 100));
+		primary.setBackground(new Color(0xFCFCFE));
+		
+		JPanel Nested0 = new JPanel();
+		JPanel Nested1 = new JPanel();	
+		Nested0.setBackground(new Color(0xFCFCFE));
+		Nested1.setBackground(new Color(0xFCFCFE));
+		
+		Nested0.add(Name_Label);
+		Nested0.add(Name_textfield);
+		Nested1.add(Qtt_Label);
+		Nested1.add(Qtt_textfield);	
+		
+		primary.add(Nested0);	
+		primary.add(Nested1);	
+		primary.add(SAVE_btn);	
+		
+		//final 
+		ItemSetUp.getContentPane().add(primary);
+		ItemSetUp.pack();
+		ItemSetUp.setVisible(true);
+				
+	}
+	void btnClick(JTextField Name_textfield, JTextField Qtt_textfield,JFrame ItemSetUp) {
+		
+		String Name = Name_textfield.getText();
+		String Qtt = Qtt_textfield.getText();
+		
+		//throwing exception
+		//Verify Name
+		if (Name.isEmpty()) {
+			JOptionPane.showMessageDialog(null,"Name cannot be empty","Error", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		//Verify Qtt
+		if (Qtt.isEmpty()) {
+			JOptionPane.showMessageDialog(null,"Qtt cannot be empty","Error", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		int INtqtt; //  the one we send to the database
+		try {INtqtt = Integer.parseInt(Qtt);}
+		catch(NumberFormatException ex) {
+			JOptionPane.showMessageDialog(null,ex+" : "+Qtt+" need to be an int ","Error", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		//close ADD Item Window
+		ItemSetUp.dispose();
+		JOptionPane.showMessageDialog(null,"save "+Name+" and "+INtqtt+" into the data BASE ");
+	}
+}
+//endregion 
+
+//LEFT SIDE BASE =================================================================================
+//Instead of creating 6 panels - just one class to auto object 
+
+//region ITEMPanel ---------------------------------------------------------------------------------------------
+
+class ItemPanel extends JPanel{	
+	
+	ItemPanel(String ItemName) {
+		setBackground(new Color(0xE2E6F5));
+		
+		JButton button = new JButton(ItemName);
+		JLabel quantityLabel = new JLabel("nbr", SwingConstants.CENTER);
+		JLabel spacerL = new JLabel ("    ", SwingConstants.CENTER);
+		JLabel spacerR = new JLabel ("    ", SwingConstants.CENTER);
+
+        button.setBackground(new Color(0xE2E6F5));
+        button.setFocusPainted(false);
+        button.setBorderPainted(false);
+
+        button.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						JOptionPane.showMessageDialog(null,"Next-open Item Window");
+					}
+				}			
+			);
+        add(spacerL);
+        add(button);
+        add(spacerR);
+        add(quantityLabel);
+	}
+}
+//endregion
+
+class REFRESH_btn extends JButton{ //Style class - because all button have the same class 
+	
+	REFRESH_btn(){
+		super("REFRESH");
+		
+		setHorizontalTextPosition(SwingConstants.CENTER);
+		setBackground(new Color(0xFCFCFE));
+		setFocusPainted(false);
+		setBorderPainted(false);
+		//--------------
+		addActionListener(
+			new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JOptionPane.showMessageDialog(null,"REFRESH -Lower stock Items");
+				}
+			}			
+		);	
+	}
+}
+
+//RIGHT SIDE BASE =================================================================================
+
+class Right_btn extends JButton{ //Style class - because all button have the same class 
+	
+	Right_btn(String btnName){
+		super(btnName); //give name to btn
+		
+		setHorizontalTextPosition(SwingConstants.CENTER);
+		setBackground(new Color(0xE2E6F5));
+		setFocusPainted(false);
+		setBorderPainted(false);
+
+		addActionListener(e -> BtnClick()); //call event
+	}
+	void BtnClick(){} //set-up event in child- each extension catch it's own
+}
+
+//BUTTON SET UP ---------------------------------------------------------------------------------
+//ADD
+class ADD_btn extends Right_btn{
+	ADD_btn() { super("ADD");} //gives back name
+	
+	void BtnClick() {
+		Item newItem = new Item();
+	}
+}
+//REMOVE
+class REMOVE_btn extends Right_btn{
+	REMOVE_btn() { super("REMOVE");} //gives back name
+	
+	void BtnClick() {
+		JOptionPane.showMessageDialog(null,"REMOVE- open page ask witch item to remove");
+	}
+}
+//EXIT 
+class EXIT_btn extends Right_btn{
+	EXIT_btn() { super("EXIT");} //gives back name
+	
+	void BtnClick() {
+		JOptionPane.showMessageDialog(null,"Close everything and save in database");
+	}
+}
+
+
+public class FactoryInventoryToolManagement {
+	public static void main(String[] args){
+		
+		//CONNECTION TO SQL ====================================================
+		//CONNECTsql database = new CONNECTsql();
+		
+		//INTERFACE ============================================================
+		//BASE-----------------
+		JFrame mainFrame = new JFrame ("INVENTORY");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setLocation(500, 300); 
+		
+		
+		//LEFT PANEL=============================================================
+		JPanel LeftPanel = new JPanel();
+		LeftPanel.setBackground(new Color(0xFCFCFE));
+		LeftPanel.setPreferredSize(new Dimension (375, 300));
+		LeftPanel.setLayout(new GridLayout(3, 3, 20,20));
+				
+		
+		// add 6 nested panel in Left panel grid layout (best for 6 + (1,2) refresh btn ) 
+		//1---------------------------
+		ItemPanel Item1 = new ItemPanel("Item");
+		ItemPanel Item2 = new ItemPanel("Item");
+		ItemPanel Item3 = new ItemPanel("Item");
+		ItemPanel Item4 = new ItemPanel("Item");
+		ItemPanel Item5 = new ItemPanel("Item");
+		ItemPanel Item6 = new ItemPanel("Item");
+		
+		REFRESH_btn btn_Refresh = new REFRESH_btn();
+			
+		//Empty - make things pretty
+		JPanel Null = new JPanel();
+		Null.setBackground(new Color(0xFCFCFE));
+		
+		//- ADD to Left Panel ----------------
+		LeftPanel.add(Null, 0,0);
+		LeftPanel.add(btn_Refresh, 0,1);
+		
+		LeftPanel.add(Item2, 1,0);
+		LeftPanel.add(Item3, 2,0);
+		LeftPanel.add(Item4, 0,1);
+		LeftPanel.add(Item5, 1,1);
+		LeftPanel.add(Item6, 2,1);
+		LeftPanel.add(Item1, 1,2);
+		
+		//RIGHT PANEL=============================================================
+		JPanel RightPanel = new JPanel();
+		RightPanel.setBackground(new Color(0xE2E6F5));
+		RightPanel.setPreferredSize(new Dimension (125, 300));
+		
+		//JComboBox(E[] items): Creates a JComboBox populated with items from an array.
+		//take item list read in MySQL storage - if any 
+		JComboBox ItemList = new JComboBox(); 
+		
+		ADD_btn btn_ADD = new ADD_btn();
+		REMOVE_btn btn_REMOVE = new REMOVE_btn();
+		EXIT_btn btn_EXIT = new EXIT_btn();
+						
+		RightPanel.add(ItemList);
+		RightPanel.add(btn_ADD);
+		RightPanel.add(btn_REMOVE);
+		RightPanel.add(btn_EXIT);
+		
+		//PRIMARY PANEL===============================================================
+		JPanel nestedprimary = new JPanel();
+		nestedprimary.setBackground(new Color(0xFCFCFE));
+		nestedprimary.add(LeftPanel);
+		nestedprimary.add(RightPanel);
+				
+		
+		//final 
+		mainFrame.getContentPane().add(nestedprimary);
+		mainFrame.pack();
+		mainFrame.setVisible(true);
+		   
+	}
+}
